ccefa446bf3344b64fe90bb2e408ab58b7992317
Alex Zhang <alexgzhang92@gmail.com>
2018-06-21 23:23:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @num = params[:number].to_i
     @phrase = params[:phrase]
-    4.times do |num|
+    @num.times do |num|
       print @phrase
     end
   end
