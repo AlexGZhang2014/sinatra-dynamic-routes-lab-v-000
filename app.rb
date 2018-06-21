04a03ddf10d8426964172ceb4a9ecbb13bf8106a
Alex Zhang <alexgzhang92@gmail.com>
2018-06-21 23:45:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -25,7 +25,7 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     @sentence = []
     params.each_value {|word| @sentence << word}
-    @sentence.join(" ")
+    @sentence.join(" ") + "."
   end
   
   
